c7e85cc099761890833f4113c67c8422738276c9
刘灯 <deng.liu@tongdun.net>
2019-01-08 20:30:58 +0800

    提交

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,10 +1,8 @@
-import { ServerResponse } from './../app/util/serverResponse.d';
-import { ServerResponse } from 'http';
+
 
 import 'egg';
 import { SequelizeStatic, Sequelize } from 'sequelize';
 import { Redis } from 'ioredis';
-import { ServerResponse } from '../app/util/serverResponse.d';
 
 declare module 'egg' {
     interface Application {
